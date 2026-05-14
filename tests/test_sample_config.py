728a11f0ad4b54550bbe1b900d2a796f3739b7d5
HoshimiRIN <admin@sakuramoe.dev>
2026-05-14 22:01:07 +0800

    chore: update sample config participants

--- tests/test_sample_config.py
+++ tests/test_sample_config.py
@@ -1,26 +1,22 @@
 from dango_sim.sample_config import build_sample_config
 from dango_sim.skills import (
-    AugustaSkill,
-    CalcharoSkill,
     ChangliSkill,
-    IunoSkill,
-    JinhsiSkill,
     LuukHerssenSkill,
+    LynaeSkill,
+    MornyeSkill,
     PhoebeSkill,
     PhrolovaSkill,
 )
 
 
-def test_sample_config_includes_new_dango_skills():
+def test_sample_config_includes_active_dango_skills():
     config = build_sample_config()
 
     skills_by_id = {dango.id: dango.skill for dango in config.participants}
 
-    assert isinstance(skills_by_id["augusta"], AugustaSkill)
-    assert isinstance(skills_by_id["iuno"], IunoSkill)
+    assert isinstance(skills_by_id["lynae"], LynaeSkill)
+    assert isinstance(skills_by_id["mornye"], MornyeSkill)
     assert isinstance(skills_by_id["phrolova"], PhrolovaSkill)
     assert isinstance(skills_by_id["changli"], ChangliSkill)
-    assert isinstance(skills_by_id["jinhsi"], JinhsiSkill)
-    assert isinstance(skills_by_id["calcharo"], CalcharoSkill)
     assert isinstance(skills_by_id["phoebe"], PhoebeSkill)
     assert isinstance(skills_by_id["luuk_herssen"], LuukHerssenSkill)
